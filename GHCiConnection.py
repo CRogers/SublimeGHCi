9cffd549c03fae4677c418092de4b101bb10a462
Callum Rogers <callumrogers@hotmail.co.uk>
2014-10-01 12:26:55 +0100

    Use double underscore to follow python convention

--- GHCiConnection.py
+++ GHCiConnection.py
@@ -6,12 +6,12 @@ prompt = (prompt_repeating_part + prompt_repeating_part[:-1]).decode('utf-8')
 
 class GHCiConnection(object):
 	def __init__(self):
-		self._sp = subprocess.Popen("/usr/local/bin/ghci", stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
+		self.__sp = subprocess.Popen("/usr/local/bin/ghci", stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-	def _read_until_prompt(self):
+	def __read_until_prompt(self):
 		data = b''
 		while True:
-			read = self._sp.stdout.read(len(prompt_repeating_part))
+			read = self.__sp.stdout.read(len(prompt_repeating_part))
 			if read == prompt_repeating_part:
 				break
 			data += read
@@ -19,11 +19,11 @@ class GHCiConnection(object):
 		return re.sub(r'^\]*((.|\n)+)\n\]*$', r'\1', string)
 
 	def message(self, msg):
-		stdin = self._sp.stdin
+		stdin = self.__sp.stdin
 		stdin.write(msg.encode('utf-8') + b'\n')
 		stdin.flush()
-		return self._read_until_prompt()
+		return self.__read_until_prompt()
 
 	def consume_beginning(self):
-		print(self._sp.stdout.read1(1000000))
+		print(self.__sp.stdout.read1(1000000))
 		print(self.message(':set prompt ' + prompt))
--- OutputPanel.py
+++ OutputPanel.py
@@ -10,29 +10,29 @@ class SublimeGhciOutputText(sublime_plugin.TextCommand):
 
 class OutputPanel(object):
 	def __init__(self, name):
-		self._name = name
-		self._output_panel = None
+		self.__name = name
+		self.__output_panel = None
 
-	def _create_output_panel(self):
-		self._output_panel = sublime.active_window().create_output_panel(self._name)
+	def __create_output_panel(self):
+		self.__output_panel = sublime.active_window().create_output_panel(self.__name)
 
-	def _get_view(self):
-		if self._output_panel == None:
-			self._create_output_panel()
-		return self._output_panel
+	def __get_view(self):
+		if self.__output_panel == None:
+			self.__create_output_panel()
+		return self.__output_panel
 
 	def display_text(self, text):
-		view = self._get_view()
+		view = self.__get_view()
 		view.set_read_only(False)
 		view.run_command('sublime_ghci_output_text', {'text': text})
 		view.set_read_only(True)
 		self.show()
 
-	def _runCommandWithPanel(self, command):
-		sublime.active_window().run_command(command, {'panel': 'output.' + self._name})
+	def __runCommandWithPanel(self, command):
+		sublime.active_window().run_command(command, {'panel': 'output.' + self.__name})
 
 	def show(self):
-		self._runCommandWithPanel('show_panel')
+		self.__runCommandWithPanel('show_panel')
 
 	def hide(self):
-		self._runCommandWithPanel('hide_panel')
+		self.__runCommandWithPanel('hide_panel')
