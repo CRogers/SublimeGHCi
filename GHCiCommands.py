138d65ee00890e042c32094975fabc94b2d07ca3
Callum Rogers <callumrogers@hotmail.co.uk>
2014-10-01 12:22:20 +0100

    Remove get from names

--- GHCiCommands.py
+++ GHCiCommands.py
@@ -30,17 +30,17 @@ class GHCiCommands(object):
 			.from_bool(is_defined, response)
 			.map(get_info_part))
 
-	def get_type(self, expr):
+	def type_of(self, expr):
 		return self.__expr_command('t', expr)
 
-	def get_kind(self, expr):
+	def kind_of(self, expr):
 		return self.__expr_command('k', expr)
 
-	def get_type_or_kind(self, sig):
+	def type_or_kind_of(self, sig):
 		last_part = get_last_part(sig) 
 		return (self
-			.get_type(sig)
-			.or_else(lambda: self.get_kind(sig)))
+			.type_of(sig)
+			.or_else(lambda: self.kind_of(sig)))
 
 	def load_haskell_file(self, file_name):
 		msg = ':load "{}"'.format(file_name)
