f067d5282a11edf049c5ffaac174bbd99e80bba0
Callum Rogers <callumrogers@hotmail.co.uk>
2014-10-04 22:01:52 +0100

    Highlight errors

--- ErrorReporter.py
+++ ErrorReporter.py
@@ -1,13 +1,42 @@
+import sublime, re
+
 from SublimeGHCi.OutputPanel import OutputPanel
+from SublimeGHCi.Highlights import ErrorHighlights
+
+class ErrorPos(object):
+	def __init__(self, file_name, row, col):
+		self.__view = sublime.active_window().find_open_file(file_name)
+		start = self.__view.text_point(row, col)
+		line = self.__view.line(start)
+		self.__region = sublime.Region(start, line.end())
+
+	def view(self):
+		return self.__view
+
+	def region(self):
+		return self.__region
+
+def match_to_error_pos(match):
+	file_name = match.group(1)
+	row = int(match.group(2)) - 1
+	col = int(match.group(3)) - 1
+	return ErrorPos(file_name, row, col)
+
+def parse_errors(error_message):
+	matches = re.finditer(r'(.*?):(\d+):(\d+):\n', error_message)
+	return map(match_to_error_pos, matches)
 
 class ErrorReporter(object):
 	def __init__(self):
 		self.__output_panel = OutputPanel('sublime_ghci')
+		self.__error_highlights = ErrorHighlights()
 
 	def report_errors(self, error_message):
 		self.__output_panel.display_text(error_message)
-		pass
+		error_positions = parse_errors(error_message)
+		self.__error_highlights.highlight(error_positions)
 
 	def clear_errors(self):
 		self.__output_panel.hide()
+		self.__error_highlights.erase()
 		pass
--- Highlights.py
+++ Highlights.py
@@ -1,14 +1,27 @@
-import sublime, uuid
+import sublime, uuid, itertools
 
 class ErrorHighlights(object):
-	def __init__(self, view):
+	def __init__(self):
+		self.__highlights = []
+
+	def erase(self):
+		for highlight in self.__highlights:
+			highlight.erase()
+
+	def highlight(self, error_positions):
+		self.erase()
+		for error_pos in error_positions:
+			self.__highlights.append(ViewErrorHighlights(error_pos.view(), [error_pos.region()]))
+
+class ViewErrorHighlights(object):
+	def __init__(self, view, regions):
 		self.__view = view
 		self.__key = str(uuid.uuid4())
 		self.__add(regions)
 
 	def __add(self, regions):
-		flags = sublime.PERSISTENT | sublime.DRAW_NO_FILL
-		self.__view.add_regions(self.__key, regions, 'keyword', 'dot', flags)
+		flags = sublime.DRAW_NO_FILL
+		self.__view.add_regions(self.__key, regions, 'keyword', '', flags)
 
 	def erase(self):
 		self.__view.erase_regions(self.__key)
