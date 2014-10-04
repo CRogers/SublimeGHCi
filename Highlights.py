13d2d66800456d70f06e5c4eae2d9a5296b0c82d
Callum Rogers <callumrogers@hotmail.co.uk>
2014-10-04 10:06:12 +0100

    Add ErrorHighlights class

---
+++ Highlights.py
@@ -0,0 +1,14 @@
+import sublime, uuid
+
+class ErrorHighlights(object):
+	def __init__(self, view, regions):
+		self.__view = view
+		self.__key = str(uuid.uuid4())
+		self.__add(regions)
+
+	def __add(self, regions):
+		flags = sublime.PERSISTENT | sublime.DRAW_NO_FILL
+		self.__view.add_regions(self.__key, regions, 'keyword', 'dot', flags)
+
+	def erase(self):
+		self.__view.erase_regions(self.__key)
