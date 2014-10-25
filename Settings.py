22066f2174a30912c5295c5c62aaeeff76119788
Callum Rogers <callumrogers@hotmail.co.uk>
2014-10-25 15:24:15 +0100

    Added settings for running tests

--- Settings.py
+++ Settings.py
@@ -8,6 +8,15 @@ class Settings(object):
 	def ghci_command(self):
 		return self.__settings.get('ghci_command')
 
+	def __ghci_test(self):
+		return self.__settings.get('ghci_test')
+
+	def test_module(self):
+		return self.__ghci_test()['module']
+
+	def test_command(self):
+		return self.__ghci_test()['command']
+
 	def project_directory(self):
 		project_file = self.__view.window().project_file_name()
 		if project_file == None:
