77bb587e6716eb7dcc97a6b210c3124f3b793db5
Callum Rogers <callumrogers@hotmail.co.uk>
2014-11-23 12:07:39 +0000

    Refactor deepest folder into method

--- projects/ProjectManager.py
+++ projects/ProjectManager.py
@@ -6,6 +6,17 @@ class ProjectManager(object):
 	def __init__(self, window_info):
 		self._window_info = window_info
 
+	def _get_deepest_folder(self, folders, directory_of_view):
+		deepest_folder_prefix_len = 0
+		deepest_folder = None
+		for folder in folders:
+			prefix = os.path.commonprefix([folder, directory_of_view])
+			if len(prefix) > deepest_folder_prefix_len:
+				deepest_folder_prefix_len = len(prefix)
+				deepest_folder = folder
+
+		return deepest_folder
+
 	def project_for_view(self, view):
 		folders = self._window_info.folders()
 		directory_of_view = os.path.dirname(view.file_name())
@@ -13,12 +24,5 @@ class ProjectManager(object):
 		if len(folders_view_is_in) == 0:
 			return Project('ghci', directory_of_view)
 		
-		deepest_folder_prefix_len = 0
-		deepest_folder = None
-		for folder in folders_view_is_in:
-			prefix = os.path.commonprefix([folder, directory_of_view])
-			if len(prefix) > deepest_folder_prefix_len:
-				deepest_folder_prefix_len = len(prefix)
-				deepest_folder = folder
-
+		deepest_folder = self._get_deepest_folder(folders_view_is_in, directory_of_view)
 		return Project('ghci', deepest_folder)
