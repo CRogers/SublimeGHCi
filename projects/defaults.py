d9d1977f1b39c4d337e48545b16ee6b0f1ffc124
Callum Rogers <callumrogers@hotmail.co.uk>
2014-11-23 23:55:19 +0000

    Tests for ProjectFileDetector

--- projects/defaults.py
+++ projects/defaults.py
@@ -1,8 +1,9 @@
 import sublime
+import glob
 
 from SublimeGHCi.projects.ProjectManager import *
 from SublimeGHCi.projects.WindowInfo import *
 from SublimeGHCi.projects.ProjectFileDetector import *
 
 def default_project_manager():
-	return ProjectManager(WindowInfo(sublime), ProjectFileDetector())
+	return ProjectManager(WindowInfo(sublime), ProjectFileDetector(glob))
