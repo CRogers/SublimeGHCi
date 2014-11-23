08ae7c05dab52de5eca6dcfe98d2a48d307374aa
Callum Rogers <callumrogers@hotmail.co.uk>
2014-11-23 17:40:53 +0000

    No folders in one window should result in no folders overall

--- tests/WindowInfoSpec.py
+++ tests/WindowInfoSpec.py
@@ -6,6 +6,11 @@ from SublimeGHCi.projects.WindowInfo import *
 class Sublime(object):
 	pass
 
+class SublimeWindow(object):
+	def __init__(self, project_data={}, folders=[]):
+		self.project_data = Mock(return_value=project_data)
+		self.folders = Mock(return_value=folders)
+
 class WindowInfoSpec(unittest.TestCase):
 	def setUp(self):
 		self.windows = Mock(return_value=[])
@@ -14,5 +19,10 @@ class WindowInfoSpec(unittest.TestCase):
 		self.window_info = WindowInfo(sublime)
 
 	def test_when_there_are_no_windows_there_should_be_no_folders(self):
+		folders = self.window_info.folders()
+		self.assertEqual(folders, [])
+
+	def test_when_there_is_one_window_with_no_folders_and_no_project_settings_there_should_be_no_folders(self):
+		self.windows.return_value = [SublimeWindow()]
 		folders = self.window_info.folders()
 		self.assertEqual(folders, [])
