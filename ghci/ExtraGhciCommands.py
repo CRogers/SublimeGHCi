f68a82c0a6ab7067af95f465567e14a611014ba0
Callum Rogers <callumrogers@hotmail.co.uk>
2014-11-24 21:57:47 +0000

    Add forwarding methods and refactor

--- ghci/ExtraGhciCommands.py
+++ ghci/ExtraGhciCommands.py
@@ -9,6 +9,27 @@ class ExtraGhciCommands(object):
 
 	def type_or_kind_of(self, sig):
 		last_part = get_last_part(sig) 
-		return (self._commands
+		return (self
 			.type_of(sig)
-			.or_else(lambda _: self._commands.kind_of(sig)))
+			.or_else(lambda _: self.kind_of(sig)))
+
+	def close(self):
+		self._commands.close()
+
+	def loaded(self):
+		self._commands.loaded()
+
+	def completions(self, prefix):
+		return self._commands.completions(prefix)
+
+	def type_of(self, expr):
+		return self._commands.type_of(expr)
+
+	def kind_of(self, expr):
+		return self._commands.kind_of(expr)
+
+	def load_haskell_file(self, file_name):
+		return self._commands.load_haskell_file(file_name)
+
+	def run_expr(self, expr):
+		return self._commands.run_expr(expr)
