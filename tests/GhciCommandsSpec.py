fa6a979d6306127b637ca0bdc65dfc8d37aa6990
Callum Rogers <callumrogers@hotmail.co.uk>
2014-11-24 01:03:46 +0000

    Tests for completions

--- tests/GhciCommandsSpec.py
+++ tests/GhciCommandsSpec.py
@@ -12,6 +12,7 @@ class LoadedGhciCommandsSpec(unittest.TestCase):
 		self.connection = GhciConnection()
 		self.connection.terminate = Mock()
 		self.connection.loaded = Mock(return_value=True)
+		self.connection.message = Mock(return_value='')
 		self.commands = GhciCommands(self.connection)
 
 	def test_when_close_is_called_terminate_is_called_on_connection(self):
@@ -24,3 +25,19 @@ class LoadedGhciCommandsSpec(unittest.TestCase):
 
 	def test_when_connection_loaded_returns_true_loaded_returns_true(self):
 		self.assertTrue(self.commands.loaded())
+
+	def test_when_calling_completions_with_prefix_should_send_appropriate_completions_command(self):
+		self.commands.completions('a')
+		self.connection.message.assert_called_once_with(':complete repl 1000000 "a"')
+
+	def test_when_completions_have_no_results_should_return_no_results(self):
+		self.connection.message.return_value = '0 0 ""'
+		completions = self.commands.completions('a')
+		self.assertTrue(completions.successful())
+		self.assertEqual(completions.value(), [])
+
+	def test_when_completions_has_two_results_should_return_a_successful_result_with_right_values(self):
+		self.connection.message.return_value = '2 2 ""\n"abc"\n"abb"'
+		completions = self.commands.completions('a')
+		self.assertTrue(completions.successful())
+		self.assertEqual(completions.value(), ['abc', 'abb'])
