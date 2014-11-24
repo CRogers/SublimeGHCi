9b26a3772ef1b2d2899bf0749ab84cf55f02240c
Callum Rogers <callumrogers@hotmail.co.uk>
2014-11-24 00:34:25 +0000

    Tests for type_of

--- tests/LoadedGhciCommandsSpec.py
+++ tests/LoadedGhciCommandsSpec.py
@@ -11,7 +11,8 @@ class LoadedGhciCommandsSpec(unittest.TestCase):
 	def setUp(self):
 		self.commands = GhciCommands()
 		self.commands.loaded = Mock(return_value=False)
-		self.commands.completions = Mock(return_value=[])
+		self.commands.completions = Mock(return_value=Fallible.succeed([]))
+		self.commands.type_of = Mock(return_value=Fallible.succeed('type'))
 		self.loaded_commands = LoadedGhciCommands(self.commands)
 
 	def test_when_inner_commands_loaded_returns_false_loaded_returns_false(self):
@@ -32,3 +33,14 @@ class LoadedGhciCommandsSpec(unittest.TestCase):
 		completions = self.loaded_commands.completions('a')
 		self.assertTrue(completions.successful())
 		self.assertEqual(completions.value(), ['ab'])
+
+	def test_when_commands_are_not_loaded_type_of_is_failed(self):
+		completions = self.loaded_commands.type_of('a')
+		self.assertTrue(completions.failed())
+
+	def test_when_commands_are_loaded_type_of_is_successful_and_returns_the_inner_value(self):
+		self.commands.loaded.return_value = True
+		self.commands.type_of.return_value = Fallible.succeed('cat')
+		completions = self.loaded_commands.type_of('a')
+		self.assertTrue(completions.successful())
+		self.assertEqual(completions.value(), 'cat')
