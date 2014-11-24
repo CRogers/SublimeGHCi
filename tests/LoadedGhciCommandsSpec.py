d58b3587f70f07633164ff3cc759308c315e8680
Callum Rogers <callumrogers@hotmail.co.uk>
2014-11-24 00:29:16 +0000

    Tests for completions

--- tests/LoadedGhciCommandsSpec.py
+++ tests/LoadedGhciCommandsSpec.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import *
 
+from SublimeGHCi.common.Fallible import *
 from SublimeGHCi.ghci.LoadedGhciCommands import *
 
 class GhciCommands(object):
@@ -10,6 +11,7 @@ class LoadedGhciCommandsSpec(unittest.TestCase):
 	def setUp(self):
 		self.commands = GhciCommands()
 		self.commands.loaded = Mock(return_value=False)
+		self.commands.completions = Mock(return_value=[])
 		self.loaded_commands = LoadedGhciCommands(self.commands)
 
 	def test_when_inner_commands_loaded_returns_false_loaded_returns_false(self):
@@ -18,3 +20,15 @@ class LoadedGhciCommandsSpec(unittest.TestCase):
 	def test_when_inner_commands_loaded_returns_true_loaded_returns_true(self):
 		self.commands.loaded.return_value = True
 		self.assertTrue(self.loaded_commands.loaded())
+
+	def test_when_commands_not_loaded_completions_is_failed_and_empty(self):
+		completions = self.loaded_commands.completions('a')
+		self.assertTrue(completions.failed())
+		self.assertEqual(completions.value(), [])
+
+	def test_when_commands_are_loaded_completions_is_successful_and_returns_the_inner_completions(self):
+		self.commands.loaded.return_value = True
+		self.commands.completions.return_value = Fallible.succeed(['ab'])
+		completions = self.loaded_commands.completions('a')
+		self.assertTrue(completions.successful())
+		self.assertEqual(completions.value(), ['ab'])
