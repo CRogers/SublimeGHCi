57a306e1935fa61afe3019fce40bce98cc21cb13
Callum Rogers <callumrogers@hotmail.co.uk>
2014-11-25 22:53:52 +0000

    Test when scope contains string

--- tests/NoStringCompletorSpec.py
+++ tests/NoStringCompletorSpec.py
@@ -12,12 +12,16 @@ class View():
 class CompletorSpec(unittest.TestCase):
 	def setUp(self):
 		self.completor = Completor()
-		self.completor.complete = Mock(return_value=[])
+		self.completor.complete = Mock(return_value=['cat'])
 		self.view = View()
 		self.view.scope_name = Mock(return_value='scope')
 		self.no_string_completor = NoStringCompletor(self.completor, self.view)
 
 	def test_when_scope_does_not_contain_string_it_should_return_the_input(self):
-		self.completor.complete.return_value = ['cat']
+		completions = self.no_string_completor.complete('c')
+		self.assertEqual(completions, ['cat'])
+
+	def test_when_scope_does_contain_string_it_should_return_no_results(self):
+		self.view.scope_name.return_value = 'source.haskell string.quoted.double.haskell '
 		completions = self.no_string_completor.complete('c')
 		self.assertEqual(completions, ['cat'])
