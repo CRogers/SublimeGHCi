652129aa4d55cf446e0a1b3becd0c2065cd228a6
Callum Rogers <callumrogers@hotmail.co.uk>
2014-11-25 23:32:05 +0000

    Added Completor to defaults

--- completions/defaults.py
+++ completions/defaults.py
@@ -1,5 +1,6 @@
+from SublimeGHCi.completions.Completor import *
 from SublimeGHCi.completions.ModulePrefixCompletor import *
 from SublimeGHCi.completions.NoStringCompletor import *
 
 def default_completor(ghci, view):
-	return NoStringCompletor(ModulePrefixCompletor(ghci, view), view)
+	return NoStringCompletor(ModulePrefixCompletor(Completor(ghci), view), view)
