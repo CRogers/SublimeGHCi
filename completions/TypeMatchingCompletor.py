f6ebfd2071db2cabe3b0fe410d60c08d02c3b729
Callum Rogers <callumrogers@hotmail.co.uk>
2014-12-06 16:23:18 +0000

    Exploratory work around TypeMatchingCompletor

--- completions/TypeMatchingCompletor.py
+++ completions/TypeMatchingCompletor.py
@@ -1,3 +1,7 @@
 class TypeMatchingCompletor(object):
+	def __init__(self, completor, view):
+		self._completor = completor
+		self._view = view
+
 	def complete(self, prefix, location):
 		return []
--- tests/TypeMatchingCompletorSpec.py
+++ tests/TypeMatchingCompletorSpec.py
@@ -7,10 +7,15 @@ class View(object):
 	def __init__(self):
 		self.text = 'test'
 
+class TypedCompletor(object):
+	def __init__(self):
+		self.complete_with_types = Mock(return_value = [])
+
 class TypeMatchingCompletorSpec(unittest.TestCase):
 	def setUp(self):
 		self.view = View()
-		self.type_matching_completor = TypeMatchingCompletor()
+		self.completor = TypedCompletor()
+		self.type_matching_completor = TypeMatchingCompletor(self.completor, self.view)
 
 	def test_when_the_cursor_is_at_a_position_where_inserting_text_would_fail_compilation_no_completions_are_returned(self):
 		self.view.text = 'import Prelude (map) '
@@ -20,3 +25,6 @@ class TypeMatchingCompletorSpec(unittest.TestCase):
 	def test_when_there_are_no_completions_then_no_completions_are_produced(self):
 		completions = self.type_matching_completor.complete('', 4)
 		self.assertEqual(completions, [])
+
+	def test_when_there_are_two_completions_but_one_of_them_fits_in_the_hole_at_cursor_put_that_one_on_top(self):
+		pass
