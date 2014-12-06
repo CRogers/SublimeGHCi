3ff106c888be8c1661193d40a069914eb16b1682
Callum Rogers <callumrogers@hotmail.co.uk>
2014-12-06 17:06:59 +0000

    Make sure load_haskell_file is called

--- ghci/TypeHoleInfoExtractor.py
+++ ghci/TypeHoleInfoExtractor.py
@@ -2,5 +2,5 @@ class TypeHoleInfoExtractor(object):
 	def __init__(self, ghci_commands):
 		self._commands = ghci_commands
 
-	def extract_info_from_at(self, text, point):
-		pass
+	def extract_info_from(self, text, point):
+		self._commands.load_haskell_file('lol')
--- tests/TypeHoleInfoExtractorSpec.py
+++ tests/TypeHoleInfoExtractorSpec.py
@@ -5,12 +5,19 @@ from SublimeGHCi.common.Fallible import *
 from SublimeGHCi.ghci.TypeHoleInfoExtractor import *
 
 class GhciCommands(object):
-	pass
+	def __init__(self):
+		self.load_haskell_file = Mock(return_value=Fallible.succeed('loaded'))
 	
 class TypeHoleInfoExtractorSpec(unittest.TestCase):
 	def setUp(self):
 		self.commands = GhciCommands()
 		self.info_extractor = TypeHoleInfoExtractor(self.commands)
 
-	def test_(self):
-		pass
+	def test_when_called_it_should_call_load_haskell_file_once(self):
+		self.info_extractor.extract_info_from('foo ', 4)
+		call_count = self.commands.load_haskell_file.call_count
+		self.assertEqual(call_count, 1)
+
+	#def test_when_called_it_should_call_load_haskell_file_and_supply_a_file_which_exists(self):
+	#	self.info_extractor.extract_info_from('foo ', 4)
+	#	self.commands.load_haskell_file.call_args
