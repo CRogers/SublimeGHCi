b850359cbe8c993b3ea12a7885ba896c16fcc2ff
Callum Rogers <callumrogers@hotmail.co.uk>
2014-12-07 21:30:39 +0000

    Wire up CachingGhciFactory in defaults

--- ghci/defaults.py
+++ ghci/defaults.py
@@ -1,9 +1,12 @@
 import tempfile, subprocess, os, threading
 
+from SublimeGHCi.ghci.CachingGhciFactory import *
+from SublimeGHCi.ghci.GhciConnectionFactory import *
 from SublimeGHCi.ghci.GhciFactory import *
 
+
 def default_ghci_factory(ghci_connection_factory):
-	return GhciFactory(tempfile, ghci_connection_factory)
+	return CachingGhciFactory(GhciFactory(tempfile, ghci_connection_factory))
 
 def default_ghci_connection_factory(project_manager):
 	return GhciConnectionFactory(subprocess, os, threading, project_manager)
