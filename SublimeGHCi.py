2d5f7d641cefa2fb3c81c02ea4a37e0c024bfebd
Callum Rogers <callumrogers@hotmail.co.uk>
2014-12-07 22:34:52 +0000

    Delete comments

--- SublimeGHCi.py
+++ SublimeGHCi.py
@@ -21,17 +21,13 @@ class HooksListener(sublime_plugin.EventListener):
 		return manager.complete(view, prefix, locations[0])
 
 	def on_new(self, view):
-		#print('new view {} created'.format(view.file_name()))
 		manager.add(view)
 
 	def on_activated(self, view):
-		#print('view {} activated'.format(view.file_name()))
 		manager.add(view)
 
 	def on_load(self, view):
-		#print('loaded view {}'.format(view.file_name()))
 		manager.add(view)
 
 	def on_close(self, view):
-		#print('view {} closed'.format(view.file_name()))
 		manager.remove(view)
