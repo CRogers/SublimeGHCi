c2ef83c5628d82eaaea8434f9be7699df2b2effa
Callum Rogers <callumrogers@hotmail.co.uk>
2014-12-07 18:57:05 +0000

    Ensure that completions are returned in order when extrating a type fails

--- completions/TypeMatchingCompletor.py
+++ completions/TypeMatchingCompletor.py
@@ -1,7 +1,8 @@
 class TypeMatchingCompletor(object):
-	def __init__(self, completor, view):
+	def __init__(self, completor, type_hole_info_extractor, view):
 		self._completor = completor
+		self._info_extractor = type_hole_info_extractor
 		self._view = view
 
-	def complete(self, prefix, location):
-		return []
+	def complete_with_types(self, prefix, location):
+		return self._completor.complete_with_types(prefix, location)
--- tests/TypeMatchingCompletorSpec.py
+++ tests/TypeMatchingCompletorSpec.py
@@ -1,15 +1,16 @@
 import unittest
 from unittest.mock import *
 
+from SublimeGHCi.common.Fallible import *
 from SublimeGHCi.completions.TypeMatchingCompletor import *
 
 class TypedCompletor(object):
 	def __init__(self):
 		self.complete_with_types = Mock(return_value = [])
 
-class ExtraGhciCommands(object):
+class TypeHoleInfoExtractor(object):
 	def __init__(self):
-		pass
+		self.type_at_point = Mock(return_value = Fallible.fail(None))
 
 class View(object):
 	def __init__(self):
@@ -19,16 +20,18 @@ class TypeMatchingCompletorSpec(unittest.TestCase):
 	def setUp(self):
 		self.view = View()
 		self.completor = TypedCompletor()
-		self.type_matching_completor = TypeMatchingCompletor(self.completor, self.view)
-
-	def test_when_the_cursor_is_at_a_position_where_inserting_text_would_fail_compilation_no_completions_are_returned(self):
-		self.view.text = 'import Prelude (map) '
-		completions = self.type_matching_completor.complete('', len(self.view.text))
-		self.assertEqual(completions, [])
+		self.info_extractor = TypedCompletor()
+		self.type_matching_completor = TypeMatchingCompletor(self.completor, self.info_extractor, self.view)
 
 	def test_when_there_are_no_completions_then_no_completions_are_produced(self):
-		completions = self.type_matching_completor.complete('', 4)
-		self.assertEqual(completions, [])
+		result = self.type_matching_completor.complete_with_types('', 4)
+		self.assertEqual(result, [])
 
-	def test_when_there_are_two_completions_but_one_of_them_fits_in_the_hole_at_cursor_put_that_one_on_top(self):
-		pass
+	def test_when_the_type_extractor_fails_the_same_completions_are_returned(self):
+		completions = ['a', 'b']
+		self.completor.complete_with_types.return_value = completions
+		result = self.type_matching_completor.complete_with_types('', 4)
+		self.assertEqual(result, completions)
+
+	#def test_when_there_are_two_completions_but_one_of_them_fits_in_the_hole_at_cursor_put_that_one_on_top(self):
+	#	pass
