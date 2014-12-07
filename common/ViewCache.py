d92c7173585e1e84d84e941660f091a9eac123a3
Callum Rogers <callumrogers@hotmail.co.uk>
2014-12-07 22:22:12 +0000

    Make ViewCache actually cache

--- common/ViewCache.py
+++ common/ViewCache.py
@@ -1,6 +1,10 @@
 class ViewCache(object):
 	def __init__(self, creation_func):
 		self._creation_func = creation_func
+		self._view_map = dict()
 
 	def get_for_view(self, view):
-		return self._creation_func(view)
+		key = view.buffer_id()
+		if key not in self._view_map:
+			self._view_map[key] = self._creation_func(view)
+		return self._view_map[key]
--- tests/ViewCacheSpec.py
+++ tests/ViewCacheSpec.py
@@ -30,3 +30,14 @@ class ViewCacheSpec(unittest.TestCase):
 		view = View()
 		self.view_cache.get_for_view(view)
 		self.new_obj.assert_called_once_with(view)
+
+	def test_when_asking_twice_with_two_different_views_different_objs_are_returned(self):
+		first = self.view_cache.get_for_view(View())
+		second = self.view_cache.get_for_view(View())
+		self.assertNotEqual(first, second)
+
+	def test_when_asking_twice_with_the_same_view_the_same_obj_is_returned(self):
+		view = View()
+		first = self.view_cache.get_for_view(view)
+		second = self.view_cache.get_for_view(view)
+		self.assertEqual(first, second)
