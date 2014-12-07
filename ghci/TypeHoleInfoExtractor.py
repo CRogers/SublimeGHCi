090a69e0887072272d612f51ee2133e807bad76a
Callum Rogers <callumrogers@hotmail.co.uk>
2014-12-07 15:06:26 +0000

    Make sure it actually puts the hole in the right place

--- ghci/TypeHoleInfoExtractor.py
+++ ghci/TypeHoleInfoExtractor.py
@@ -1,7 +1,8 @@
 class TypeHoleInfoExtractor(object):
 	def __init__(self, ghci_commands, type_hole_name = 'sublimeghci'):
 		self._commands = ghci_commands
-		self._type_hole_name = type_hole_name
+		self._type_hole = '_' + type_hole_name
 
 	def extract_info_from(self, text, point):
-		self._commands.load_from_string('_hole')
+		new_text = text[:point] + self._type_hole + text[point:]
+		self._commands.load_from_string(new_text)
--- tests/TypeHoleInfoExtractorSpec.py
+++ tests/TypeHoleInfoExtractorSpec.py
@@ -8,14 +8,15 @@ class GhciCommands(object):
 	def __init__(self):
 		self.load_from_string = Mock(return_value=Fallible.succeed('loaded'))
 
-type_hole_name = 'hole'
-type_hole = '_' + type_hole_name
-
 class TypeHoleInfoExtractorSpec(unittest.TestCase):
 	def setUp(self):
 		self.commands = GhciCommands()
-		self.info_extractor = TypeHoleInfoExtractor(self.commands, type_hole_name)
+		self.info_extractor = TypeHoleInfoExtractor(self.commands, 'hole')
 
-	def test_when_called_calls_inner_commands_load_from_string_with_type_hole_in_given_location(self):
+	def test_when_called_calls_inner_commands_load_from_string_with_type_hole_at_beginning(self):
 		self.info_extractor.extract_info_from('', 0)
-		self.commands.load_from_string.assert_called_once_with(type_hole)
+		self.commands.load_from_string.assert_called_once_with('_hole')
+
+	def test_when_called_calls_inner_commands_load_from_string_with_type_hole_inside_text(self):
+		self.info_extractor.extract_info_from('someFunc .  . anotherFunc', 11)
+		self.commands.load_from_string.assert_called_once_with('someFunc . _hole . anotherFunc')
