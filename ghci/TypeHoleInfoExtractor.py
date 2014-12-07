0f9edb19e3435aba65b7d2b22a64ea0551ace93f
Callum Rogers <callumrogers@hotmail.co.uk>
2014-12-07 18:18:45 +0000

    Read type info from simple type hole error

--- ghci/TypeHoleInfoExtractor.py
+++ ghci/TypeHoleInfoExtractor.py
@@ -1,8 +1,20 @@
+import re
+
+from SublimeGHCi.common.Fallible import *
+
 class TypeHoleInfoExtractor(object):
 	def __init__(self, ghci_commands, type_hole_name = 'sublimeghci'):
 		self._commands = ghci_commands
 		self._type_hole = '_' + type_hole_name
 
+	def _extract_hole_type(self, error_output):
+		regex = r'Found hole ‘{}’ with type: (.*)'.format(self._type_hole)
+		match = re.search(regex, error_output)
+		if match == None:
+			return Fallible.fail(None)
+		return Fallible.succeed(match.group(1))
+
 	def extract_info_from(self, text, point):
 		new_text = text[:point] + self._type_hole + text[point:]
-		self._commands.load_from_string(new_text)
+		error_output = self._commands.load_from_string(new_text).value()
+		return self._extract_hole_type(error_output)
--- tests/TypeHoleInfoExtractorSpec.py
+++ tests/TypeHoleInfoExtractorSpec.py
@@ -6,7 +6,7 @@ from SublimeGHCi.ghci.TypeHoleInfoExtractor import *
 
 class GhciCommands(object):
 	def __init__(self):
-		self.load_from_string = Mock(return_value=Fallible.succeed('loaded'))
+		self.load_from_string = Mock(return_value=Fallible.fail('failed'))
 
 class TypeHoleInfoExtractorSpec(unittest.TestCase):
 	def setUp(self):
@@ -20,3 +20,36 @@ class TypeHoleInfoExtractorSpec(unittest.TestCase):
 	def test_when_called_calls_inner_commands_load_from_string_with_type_hole_inside_text(self):
 		self.info_extractor.extract_info_from('someFunc .  . anotherFunc', 11)
 		self.commands.load_from_string.assert_called_once_with('someFunc . _hole . anotherFunc')
+
+	def test_when_load_is_unsuccessful_return_the_type_of_the_hole_simple(self):
+		self.commands.load_from_string.return_value = Fallible.fail('''[1 of 1] Compiling X          ( /Users/X.hs, interpreted )
+
+/Users/X.hs:24:17:
+    Found hole ‘_hole’ with type: Int
+    Relevant bindings include
+      hlength :: HList '[] -> Int
+        (bound at /Users/X.hs:24:5)
+    In the expression: _hole
+    In an equation for ‘hlength’: hlength _ = _hole
+    In the instance declaration for ‘HLength (HList '[])’
+Failed, modules loaded: none.''')
+		type = self.info_extractor.extract_info_from('a .  . b', 4)
+		self.assertEqual(type, Fallible.succeed('Int'))
+
+	def test_when_load_is_unsuccessful_return_the_type_of_the_hole_complex(self):
+		self.commands.load_from_string.return_value = Fallible.fail('''[1 of 1] Compiling X          ( /Users/X.hs, interpreted )
+
+/Users/X.hs:40:9:
+    Found hole ‘_hole’
+      with type: HList '[Event t Int]
+                 -> Moment t (HList '[Event t [Char]])
+    Where: ‘t’ is a rigid type variable bound by
+               the type signature for
+                 proc1 :: Events t '[Int] -> Moment t (Events t '[String])
+               at /Users/X.hs:39:10
+    Relevant bindings include
+      proc1 :: Events t '[Int] -> Moment t (Events t '[String])
+        (bound at /Users/X.hs:40:1)
+    In the expression: _
+    In an equation for ‘proc1’: proc1 = _
+Failed, modules loaded: none.''')
