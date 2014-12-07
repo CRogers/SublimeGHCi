8e67f2ef66705dd5e703f515a323a1b39a4c78e7
Callum Rogers <callumrogers@hotmail.co.uk>
2014-12-07 19:20:02 +0000

    Rearrange so that types which match exactly are on top

--- completions/TypeMatchingCompletor.py
+++ completions/TypeMatchingCompletor.py
@@ -1,8 +1,26 @@
 class TypeMatchingCompletor(object):
-	def __init__(self, completor, type_hole_info_extractor, view):
+	def __init__(self, sublime, completor, type_hole_info_extractor, view):
+		self._sublime = sublime
 		self._completor = completor
 		self._info_extractor = type_hole_info_extractor
 		self._view = view
 
+	def _get_view_text(self):
+		return self._view.substr(self._sublime.Region(0, self._view.size()))
+
+	def _sort_completions_by_type(self, type, completions):
+		matching = []
+		non_matching = []
+		for x, t in completions:
+			if t == type:
+				matching.append((x, t))
+			else:
+				non_matching.append((x, t))
+		return matching + non_matching
+
 	def complete_with_types(self, prefix, location):
-		return self._completor.complete_with_types(prefix, location)
+		completions = self._completor.complete_with_types(prefix, location)
+		text = self._get_view_text()
+		return (self._info_extractor.type_at_point(text, location).switch(
+			lambda type: self._sort_completions_by_type(type, completions),
+			lambda _: completions))
--- tests/TypeMatchingCompletorSpec.py
+++ tests/TypeMatchingCompletorSpec.py
@@ -4,6 +4,10 @@ from unittest.mock import *
 from SublimeGHCi.common.Fallible import *
 from SublimeGHCi.completions.TypeMatchingCompletor import *
 
+class Sublime(object):
+	def Region(self, start, end):
+		return (start, end)
+
 class TypedCompletor(object):
 	def __init__(self):
 		self.complete_with_types = Mock(return_value = [])
@@ -16,22 +20,31 @@ class View(object):
 	def __init__(self):
 		self.text = 'test'
 
+	def substr(self, range):
+		return 'blah'
+
+	def size(self):
+		return 10
+
 class TypeMatchingCompletorSpec(unittest.TestCase):
 	def setUp(self):
 		self.view = View()
 		self.completor = TypedCompletor()
-		self.info_extractor = TypedCompletor()
-		self.type_matching_completor = TypeMatchingCompletor(self.completor, self.info_extractor, self.view)
+		self.info_extractor = TypeHoleInfoExtractor()
+		self.type_matching_completor = TypeMatchingCompletor(Sublime(), self.completor, self.info_extractor, self.view)
 
 	def test_when_there_are_no_completions_then_no_completions_are_produced(self):
 		result = self.type_matching_completor.complete_with_types('', 4)
 		self.assertEqual(result, [])
 
 	def test_when_the_type_extractor_fails_the_same_completions_are_returned(self):
-		completions = ['a', 'b']
+		completions = [('f', 'a'), ('g', 'b')]
 		self.completor.complete_with_types.return_value = completions
 		result = self.type_matching_completor.complete_with_types('', 4)
 		self.assertEqual(result, completions)
 
-	#def test_when_there_are_two_completions_but_one_of_them_fits_in_the_hole_at_cursor_put_that_one_on_top(self):
-	#	pass
+	def test_when_there_are_two_completions_but_one_of_them_to_the_type_at_cursor_put_that_one_on_top(self):
+		self.completor.complete_with_types.return_value = [('f', 'a'), ('g', 'b')]
+		self.info_extractor.type_at_point.return_value = Fallible.succeed('b')
+		result = self.type_matching_completor.complete_with_types('', 4)
+		self.assertEqual(result, [('g', 'b'), ('f', 'a')])
