8c4b95cf374dcbed133ee576d5b65f5c3febb004
Callum Rogers <callumrogers@hotmail.co.uk>
2014-12-07 22:24:53 +0000

    Refactored CachingGhciFactory to use ViewCache

--- ghci/CachingGhciFactory.py
+++ ghci/CachingGhciFactory.py
@@ -1,13 +1,12 @@
+from SublimeGHCi.common.ViewCache import *
+
 class CachingGhciFactory(object):
 	def __init__(self, ghci_factory):
 		self._ghci_factory = ghci_factory
-		self._view_to_ghci = dict()
+		self._ghci_cache = ViewCache(self._ghci_factory.ghci_for_view)
 
 	def new_type_hole_info_extractor(self):
 		return self._ghci_factory.new_type_hole_info_extractor()
 
 	def ghci_for_view(self, view):
-		key = view.buffer_id()
-		if key not in self._view_to_ghci:
-			self._view_to_ghci[key] = self._ghci_factory.ghci_for_view(view)
-		return self._view_to_ghci[key]
+		return self._ghci_cache.get_for_view(view)
