c88f0342004071d55bd16a2b3ebe0b3be3422791
Callum Rogers <callumrogers@hotmail.co.uk>
2014-12-07 20:28:23 +0000

    Redo how the factories work

--- completions/defaults.py
+++ completions/defaults.py
@@ -1,10 +1,6 @@
-from SublimeGHCi.completions.Completor import *
-from SublimeGHCi.completions.ModulePrefixCompletor import *
-from SublimeGHCi.completions.NoStringCompletor import *
-from SublimeGHCi.completions.TypeAddingCompletor import *
-from SublimeGHCi.completions.OutputCompletor import *
+import sublime
 
-def default_completor(ghci_commands, view):
-	untyped_completor = NoStringCompletor(ModulePrefixCompletor(Completor(ghci_commands), view), view)
-	typed_completor = TypeAddingCompletor(untyped_completor, ghci_commands)
-	return OutputCompletor(typed_completor)
+from SublimeGHCi.completions.CompletorFactory import *
+
+def default_completor_factory(ghci_factory):
+	return CompletorFactory(sublime, ghci_factory)
--- ghci/GhciFactory.py
+++ ghci/GhciFactory.py
@@ -1,14 +1,29 @@
-from SublimeGHCi.ghci.GhciConnection import *
 from SublimeGHCi.ghci.AutoloadingGhciConnection import *
-from SublimeGHCi.ghci.GhciCommands import *
 from SublimeGHCi.ghci.ExtraGhciCommands import *
+from SublimeGHCi.ghci.GhciCommands import *
+from SublimeGHCi.ghci.GhciConnection import *
 from SublimeGHCi.ghci.LoadedGhciCommands import *
+from SublimeGHCi.ghci.StringAcceptingGhci import *
+from SublimeGHCi.ghci.TypeHoleInfoExtractor import *
 
 class GhciFactory(object):
 	def __init__(self, project_manager):
 		self._project_manager = project_manager
 
-	def new_ghci_for_view(self, view):
+	def _new_default_connection(self, connection):
 		project = self._project_manager.project_for_view(view)
-		connection = AutoloadingGhciConnection(GhciConnection(project), view.file_name())
+		return GhciConnection(project)
+
+	def _new_ghci(self, connection):
 		return LoadedGhciCommands(ExtraGhciCommands(GhciCommands(connection)))
+
+	def _new_string_accepting_ghci(self, connection):
+		return StringAcceptingGhci(self._new_ghci(connection))
+
+	def new_type_hole_info_extractor(self):
+		ghci_commands = self._new_string_accepting_ghci(self._new_default_connection())
+		return TypeHoleInfoExtractor(ghci_commands)
+
+	def new_ghci_for_view(self, view):
+		connection = AutoloadingGhciConnection(self._new_default_connection(), view.file_name())
+		return self._new_ghci(connection)
