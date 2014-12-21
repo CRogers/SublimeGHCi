e60debcbf7f8be84c85be16c1db5a1efe7952fce
Callum Rogers <callumrogers@hotmail.co.uk>
2014-12-21 00:31:26 +0000

    Run integ test function

--- IntegTests.py
+++ IntegTests.py
@@ -1,6 +1,10 @@
 import os
 
 if os.environ.get('INTEG_TESTS') == '1':
-	print('yay')
+	name = os.environ.get('INTEG_NAME')
+	func = os.environ.get('INTEG_FUNC')
+	SublimeGHCi = __import__('SublimeGHCi.integ_tests.{}'.format(name))
+	module = getattr(SublimeGHCi.integ_tests, name)
+	getattr(module, func)()
 else:
 	print('nope')
--- integ_tests/CompletionsIntegSpec.py
+++ integ_tests/CompletionsIntegSpec.py
@@ -1,13 +1,19 @@
 import unittest
 import subprocess
-import os
+import os, sys
 
 path = '/Applications/Sublime Text.app/Contents/MacOS/Sublime Text'
+
+def print_yay():
+	print('yay2')
 
 class CompletionsIntegSpec(unittest.TestCase):
 	def setUp(self):
 		env = os.environ.copy()
+		print(__name__)
 		env['INTEG_TESTS'] = '1'
+		env['INTEG_NAME'] = __name__
+		env['INTEG_FUNC'] = 'print_yay'
 		p = subprocess.Popen([path], env=env)
 
 	def test_(self):
