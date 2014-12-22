c517e62e4c5d2012fbab412bfc888444c29a08e0
Callum Rogers <callumrogers@hotmail.co.uk>
2014-12-22 15:12:51 +0000

    Add module prefix test

--- integ_tests/CompletionsIntegSpec.py
+++ integ_tests/CompletionsIntegSpec.py
@@ -2,7 +2,7 @@ import unittest
 import time
 
 try:
-	import integ_tests.utils
+	from integ_tests.utils import run_integ_test
 except ImportError:
 	pass
 
@@ -17,6 +17,9 @@ except ImportError:
 def top_level_f():
 	return top_level_completion_with('f')
 
+def top_level_F():
+	return top_level_completion_with('F')
+
 def top_level_b():
 	return top_level_completion_with('b')
 
@@ -30,13 +33,17 @@ def top_level_completion_with(prefix):
 
 class CompletionsIntegSpec(unittest.TestCase):
 	def test_no_completions(self):
-		cat = integ_tests.utils.run_integ_test(top_level_f, 'integ_tests/Completions/NoCompletions.hs')
+		cat = run_integ_test(top_level_f, 'integ_tests/Completions/NoCompletions.hs')
 		self.assertEqual(eval(cat), [])
 
 	def test_one_completion(self):
-		result = integ_tests.utils.run_integ_test(top_level_f, 'integ_tests/Completions/OneCompletion.hs')
+		result = run_integ_test(top_level_f, 'integ_tests/Completions/OneCompletion.hs')
 		self.assertEqual(eval(result), [('foo\tFoo', 'foo')])
 
+	def test_should_suggest_only_module_prefixed_completions_after_dot(self):
+		result = run_integ_test(top_level_F, 'integ_tests/Completions/MultipleModules', 'integ_tests/Completions/MultipleModules/SecondModule.hs')
+		self.assertEqual(eval(result), [('FirstModule.bar\tFirstModule.Bar', 'FirstModule.bar')])
+
 	def test_mutliple_modules(self):
-		result = integ_tests.utils.run_integ_test(top_level_b, 'integ_tests/Completions/MultipleModules', 'integ_tests/Completions/MultipleModules/SecondModule.hs')
+		result = run_integ_test(top_level_b, 'integ_tests/Completions/MultipleModules', 'integ_tests/Completions/MultipleModules/SecondModule.hs')
 		self.assertEqual(eval(result), [('bar\tFirstModule.Bar', 'bar')])
