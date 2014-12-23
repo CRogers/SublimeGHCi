b98ba8757f86aa1c2ea9991c1926959e0cd5fd63
Callum Rogers <callumrogers@hotmail.co.uk>
2014-12-23 23:55:48 +0000

    Proper return codes from running all the tests, up circleci timeout

---
+++ run_all_tests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+shopt -s globstar
+
+function kill_dists() {
+        rm -rf **/dist/
+}
+
+kill_dists
+
+echo Running unit tests...
+./run_tests.sh
+EXIT1=$?
+echo Unit tests exited with status $EXIT1
+
+echo
+echo Running integ tests....
+./run_integ_tests.sh
+EXIT2=$?
+echo Integ tests exited with status $EXIT2
+
+kill_dists
+
+if [ "$EXIT1" == "0" ] && [ "$EXIT2" == "0" ]; then
+        EXIT_STATUS=0
+else
+        EXIT_STATUS=1
+fi
+
+echo
+echo Overall exit status: $EXIT_STATUS
+
+exit $EXIT_STATUS
--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,6 @@
 cd ..
 python3 -m unittest discover -s SublimeGHCi/tests -p *Spec.py
+EXIT_STATUS=$?
 cd SublimeGHCi
+
+exit $EXIT_STATUS
