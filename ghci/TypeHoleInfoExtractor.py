5bf5c49a3d5c83552f3216d9d12025418a7cf8d6
Callum Rogers <callumrogers@hotmail.co.uk>
2014-12-24 16:57:34 +0000

    Refactor THIE

--- ghci/TypeHoleInfoExtractor.py
+++ ghci/TypeHoleInfoExtractor.py
@@ -17,22 +17,22 @@ class TypeHoleInfoExtractor(object):
 		type_without_breaks = strip_whitespace_on_leading_lines(match.group(1))
 		return Fallible.succeed(type_without_breaks)
 
-	def _blah(self, text, start, length, holes):
+	def _hole_with_dummies(self, num_dummies):
+		return self._type_hole + ' _dummyhole' * num_dummies
+
+	def _add_holes_and_load(self, text, start, length, num_dummies):
 		end = start + length
-		new_text = text[:start] + holes + text[end:]
+		new_text = text[:start] + self._hole_with_dummies(num_dummies) + text[end:]
 		return self._commands.load_from_string(new_text).value()
 
 	def _too_few(self, error_output):
 		return re.search(r'Probable cause: ‘.*?’ is applied to too few arguments', error_output) != None
 
-	def _hole_with_dummies(self, num_dummies):
-		return self._type_hole + ' _dummyhole' * num_dummies
-
 	def _add_holes_until_it_typechecks(self, text, start, length):
 		error_output = None
 		i = 0
 		while True:
-			error_output = self._blah(text, start, length, self._hole_with_dummies(i))
+			error_output = self._add_holes_and_load(text, start, length, i)
 			i += 1
 			if i == self._max_holes_to_add or not self._too_few(error_output):
 				break
