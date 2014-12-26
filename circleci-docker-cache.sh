a6b3718479bd8b508bd749bd07a56f06e07885e4
Callum Rogers <callumrogers@hotmail.co.uk>
2014-12-26 20:20:37 +0000

    Add times

--- circleci-docker-cache.sh
+++ circleci-docker-cache.sh
@@ -7,11 +7,11 @@ DOCKER_TAG=crogers/sublimeghci-docker:latest
 mkdir -p $CACHE_DIR
 if [[ -e $IMAGE ]]; then
 	echo Loading docker image...
-	docker load -i $IMAGE
+	time docker load -i $IMAGE
 else
 	echo FROM $DOCKER_TAG >Dockerfile
 	echo Downloading docker layers...
-	docker build -t $DOCKER_TAG .
+	time docker build -t $DOCKER_TAG .
 	echo Saving docker image...
-	docker save $DOCKER_TAG >$IMAGE
+	time docker save $DOCKER_TAG >$IMAGE
 fi
