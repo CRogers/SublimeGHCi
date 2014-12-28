6601a7fa5347aaa22b94a526ad74db7970ae1257
Callum Rogers <callumrogers@hotmail.co.uk>
2014-12-28 22:53:00 +0000

    Add supremely complicated test. Damn you concurrency.

--- ghci/connection/FailedGhciConnection.py
+++ ghci/connection/FailedGhciConnection.py
@@ -1,5 +1,17 @@
+from threading import Semaphore
+
 from SublimeGHCi.common.EventHook import *
 from SublimeGHCi.common.Fallible import *
+
+class Box(object):
+	def __init__(self, value):
+		self.set(value)
+
+	def get(self):
+		return self._value
+
+	def set(self, value):
+		self._value = value
 
 class FailedGhciConnection(object):
 	def __init__(self, internal_ghci_factory, failure_reason):
@@ -12,8 +24,12 @@ class FailedGhciConnection(object):
 
 	def load_haskell_file(self, file_name):
 		loading_ghci_connection = self._internal_ghci_factory.new_loading_ghci_connection()
+		sema = Semaphore(value=0)
+		after_loading_connection = Box(None)
+		loading_ghci_connection.next.register(lambda next: [after_loading_connection.set(next), sema.release()])
 		self.next.fire(loading_ghci_connection)
-		return loading_ghci_connection.load_haskell_file(file_name)
+		sema.acquire()
+		return after_loading_connection.get().load_haskell_file(file_name)
 
 	def loaded(self):
 		return False
