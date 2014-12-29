efdab0aaaeaf4910e40afa04f5ccacabb319ee1a
Callum Rogers <callumrogers@hotmail.co.uk>
2014-12-29 16:42:18 +0000

    Create OutputPanelFactory with cache

--- output_panels/OutputPanelFactory.py
+++ output_panels/OutputPanelFactory.py
@@ -1,6 +1,9 @@
+from SublimeGHCi.common.WindowCache import *
+from SublimeGHCi.output_panels.OutputPanel import *
+
 class OutputPanelFactory(object):
 	def __init__(self):
-		pass
+		self._window_cache = WindowCache(OutputPanel)
 
-	def output_panel_for_window(self):
-		pass
+	def output_panel_for_window(self, window):
+		return self._window_cache.get(window)
