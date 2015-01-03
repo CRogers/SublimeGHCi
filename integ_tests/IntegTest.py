c72ec74ba34e54ed1a1166de47facf94a247e607
Callum Rogers <callumrogers@hotmail.co.uk>
2015-01-03 16:45:10 +0000

    Add single result to with_file

--- integ_tests/IntegTest.py
+++ integ_tests/IntegTest.py
@@ -18,18 +18,57 @@ class IntegTestContext():
 	def all_results(self):
 		return self._results
 
-class IntegTest(object):
+class Results():
+	def __init__(self):
+		self._results = []
+		self._last_result = None
+
+	def set_last_result(self, result):
+		self._last_result = result
+
+	def add_last_result(self):
+		self._results.append(self._last_result)
+
+	def all_results(self):
+		return self._results
+
+class AddResult():
+	def perform(self, results):
+		results.add_last_result()
+
+class ViewIntegTest():
 	def __init__(self):
 		self._commands = []
 
-	def _add(self, command):
+	def add_command(self, command):
 		self._commands.append(command)
+		return self
+
+	def add_result(self):
+		self.add_command(AddResult())
+		return self
+
+	def run(self, results):
+		for command in self._commands:
+			result = command.perform(results)
+			results.set_last_result(result)
+
+		return results.all_results()
+
+class IntegTest(object):
+	def __init__(self):
+		self._with_file = None
 
 	def with_file(self, with_view_test):
+		self._with_file = with_view_test(ViewIntegTest())
 		return self
 
 	def run(self):
-		return []
+		if self._with_file == None:
+			return []
+
+		results = Results()
+		return self._with_file.run(results)
 
 for name, cls in commands.__dict__.items():
 	if not inspect.isclass(cls):
---
+++ tests/IntegTestSpec.py
@@ -0,0 +1,40 @@
+import unittest
+
+from SublimeGHCi.integ_tests.IntegTest import IntegTest
+
+class Returns():
+    def __init__(self, result):
+        self._result = result
+
+    def perform(self, results):
+        return self._result
+
+class IntegTestSpec(unittest.TestCase):
+    def setUp(self):
+        self.integ_test = IntegTest()
+
+    def test_when_no_commands_are_run_it_returns_an_empty_list(self):
+        results = self.integ_test.run()
+        self.assertEqual(results, [])
+
+    def test_when_a_with_file_command_is_called_and_nothing_is_done_with_it_there_are_no_results(self):
+        results = self.integ_test.with_file(lambda x: x).run()
+        self.assertEqual(results, [])
+
+    def test_when_a_with_file_command_is_called_and_adds_a_result_the_result_is_returned_when_run(self):
+        results = (self.integ_test
+            .with_file(lambda x: x
+                .add_command(Returns(4))
+                .add_result())
+            .run())
+        self.assertEqual(results, [4])
+
+    def test_when_a_with_file_command_adds_two_results_those_two_results_are_returned(self):
+        results = (self.integ_test
+            .with_file(lambda x: x
+                .add_command(Returns(4))
+                .add_result()
+                .add_command(Returns(5))
+                .add_result())
+            .run())
+        self.assertEqual(results, [4, 5])
